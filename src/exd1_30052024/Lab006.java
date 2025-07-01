8b4e4204d18e57ca047adc4ed85ff82144c317b5
a.nayak <aloknayak2013@gmail.com>
2025-07-01 19:22:50 +0530

    Second Commit

---
+++ src/exd1_30052024/Lab006.java
@@ -0,0 +1,12 @@
+package exd1_30052024;
+
+public class Lab006 {
+    // Can I run class without a main method?
+    // run option?
+    static public void  main(String[] args) {
+
+    }
+    public static void main() {
+
+    }
+}
---
+++ src/exd1_30052024/Lab007.java
@@ -0,0 +1,15 @@
+package exd1_30052024;
+
+public class Lab007 {
+    //public static void main(String[] args) {
+    public static void main(String args[]) {
+        System.out.println("Yes");
+    }
+
+    public static void main(String args) {
+
+    }
+    public static void main(int[] args) {
+
+    }
+}
