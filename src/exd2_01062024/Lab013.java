ed0ec75c8a733611a901153f88e2932dd2dbdd39
a.nayak <aloknayak2013@gmail.com>
2025-07-02 21:14:59 +0530

    Fourth Commit

---
+++ src/exd2_01062024/Lab013.java
@@ -0,0 +1,8 @@
+package exd2_01062024;
+
+public class Lab013 {
+    public static void main(String[] args) {
+        int A123 = 987;
+        System.out.println(A123);
+    }
+}
---
+++ src/exd2_01062024/Lab017.java
@@ -0,0 +1,7 @@
+package exd2_01062024;
+
+public class Lab017 {
+    public static void main(String[] args) {
+
+    }
+}
